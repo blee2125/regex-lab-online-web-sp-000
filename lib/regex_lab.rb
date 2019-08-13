12b52148e94004bac0ee7325bfb02d329602a580
favorable-device-5158 <blee2125@gmail.com>
2019-08-13 15:17:21 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
-def starts_with_a_vowel?(word.scan(/\A[aeiou]/) { |match|  })
-
+def starts_with_a_vowel?(word)
+  if word.scan(/\A[aeiou]/) { |match|  }
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
